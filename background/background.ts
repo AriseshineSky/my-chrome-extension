3596350d2873618935bb0235c27ed5a9f7f974ec
SmileintheSky <befruitful12@gmail.com>
2025-03-29 15:58:40 -0500

    first init

---
+++ background/background.ts
@@ -0,0 +1,12 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.type === 'getOrders') {
+		const orders = [
+			{ orderId: 1, product: 'Product 1', status: 'Shipped' },
+			{ orderId: 2, product: 'Product 2', status: 'Delivered' },
+		];
+		sendResponse(orders);
+	} else if (message.type === 'updateButton') {
+		sendResponse({ active: true });  // 可以返回按钮是否激活的状态
+	}
+});
+
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'public/manifest.json',
+					dest: '.',
+				}
+			],
+		}),
+	],
+	build: {
+		outDir: 'build',
+		rollupOptions: {
+			input: {
+				main: './index.html',
+				background: './background/background.ts'
+			}
+		},
+		minify: false
+	},
+	server: {
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost',
+		}
+	}
+})
