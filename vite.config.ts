3c1933d15ea70da9f8362d6c7d2fc7e3602264ee
Sky <befruitful12@gmail.com>
2025-03-29 22:46:04 -0500

    fix manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,26 +9,30 @@ export default defineConfig({
 		viteStaticCopy({
 			targets: [
 				{
-					src: 'public/manifest.json',
-					dest: '.',
-				}
+					src: "public/manifest.json",
+					dest: ".",
+				},
 			],
 		}),
 	],
 	build: {
-		outDir: 'build',
+		outDir: "build",
 		rollupOptions: {
 			input: {
-				main: './index.html',
-				background: './background/background.ts'
-			}
+				main: "./index.html",
+				background: "./background/background.ts",
+				content: "./content/content.ts",
+			},
+			output: {
+				entryFileNames: "[name].js",
+			},
 		},
-		minify: false
+		minify: false,
 	},
 	server: {
 		hmr: {
-			protocol: 'ws',
-			host: 'localhost',
-		}
-	}
-})
+			protocol: "ws",
+			host: "localhost",
+		},
+	},
+});
