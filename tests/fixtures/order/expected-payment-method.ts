915c65a5357a198da663a0665c857e58404c4f06
SmileintheSky <befruitful12@gmail.com>
2026-02-17 09:26:54 -0600

    add payment

--- tests/fixtures/order/expected-payment-method.ts
+++ tests/fixtures/order/expected-payment-method.ts
@@ -2,4 +2,5 @@ export const EXPECTED_PAYMENT_METHOD = {
   "order/111-6784099-6345037.html": "AMEX ending in 2044",
   "order/202-6344826-2669928.html": "AMEX ending in 1038",
   "order/205-1272725-0305915.html": "AMEX ending in 2085",
+  "order/mx/702-0137443-3750617.html": "Visa que termina en 9049",
 };
